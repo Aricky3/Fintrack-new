99933ee31cad7c81a1044d1202b690007c090a05
Ricky Aguilar <r3aguila@ucsd.edu>
2025-01-11 17:17:16 -0800

    Deploy-ready: Fixed React build serving and updated dependencies

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
 const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load environment variables
+dotenv.config();
 
 const connectDb = async () => {
     mongoose.connect(process.env.MONGO_URL, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
-    .then(()=> console.log(`Database connected successfully`.bgCyan.white))
-    .catch( (error) => {
-        console.log("issue in db connection");
+    .then(() => console.log(`Database connected successfully`.bgCyan.white))
+    .catch((error) => {
+        console.log("Issue in DB connection");
         console.error(`${error}`.bgRed);
         process.exit(1);
     });
